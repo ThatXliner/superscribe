a0b59ac46330b90f7c2f53c1fc721973151f2764
Bryan Hu <bryan.hu.2020@gmail.com>
2024-03-09 23:33:02 -0800

    :sparkles: Added Mermaid graph

---
+++ src/lib/MermaidGraph.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import mermaid from 'mermaid';
+	import { onMount } from 'svelte';
+	mermaid.initialize({ startOnLoad: true });
+	const { graph }: { graph: string } = $props();
+	let ref: HTMLPreElement;
+	onMount(async () => {
+		await mermaid.run({ nodes: [ref] });
+	});
+</script>
+
+<pre bind:this={ref}>
+    {graph}
+</pre>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import MermaidGraph from '$lib/MermaidGraph.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<MermaidGraph
+	graph="
+graph LR
+A --- B
+B-->C[fa:fa-ban forbidden]
+B-->D(arst fa:fa-spinner);"
+/>
